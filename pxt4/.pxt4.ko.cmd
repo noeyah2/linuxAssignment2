b5e4ad7e9ff1c1a6f6b40d651e569ad4832c680d
noeyah2 <hy0708@cau.ac.kr>
2023-11-14 18:27:53 +0900

    save file for assignment

---
+++ pxt4/.pxt4.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/hayeon/workspacehy/pxt4/pxt4.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -z noexecstack   --build-id  -T ./scripts/module-common.lds -o /home/hayeon/workspacehy/pxt4/pxt4.ko /home/hayeon/workspacehy/pxt4/pxt4.o /home/hayeon/workspacehy/pxt4/pxt4.mod.o;  true
---
+++ pxt4/jbd3/jbd3.mod
@@ -0,0 +1,2 @@
+/home/hayeon/workspacehy/pxt4/jbd3/transaction.o /home/hayeon/workspacehy/pxt4/jbd3/commit.o /home/hayeon/workspacehy/pxt4/jbd3/recovery.o /home/hayeon/workspacehy/pxt4/jbd3/checkpoint.o /home/hayeon/workspacehy/pxt4/jbd3/revoke.o /home/hayeon/workspacehy/pxt4/jbd3/journal.o
+
